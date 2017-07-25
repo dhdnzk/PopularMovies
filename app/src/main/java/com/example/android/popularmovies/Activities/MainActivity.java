b49006a213fa5853fe3d7be72321dfc7161227f2
Donghoon Im <dongttang@gmail.com>
2017-07-25 11:08:11 +0900

    make async task innerclass

--- app/src/main/java/com/example/android/popularmovies/Activities/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.popularmovies.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -9,10 +10,16 @@ import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.android.popularmovies.AsyncTaskClasses.MovieInfoRequestingTask;
+import com.example.android.popularmovies.MovieInfo;
 import com.example.android.popularmovies.MovieListAdapter;
 import com.example.android.popularmovies.R;
+import com.example.android.popularmovies.utilities.MovieDBJsonUtils;
 import com.example.android.popularmovies.utilities.NetworkUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements MovieListAdapter.RecyclerViewClickListener {
 
@@ -26,11 +33,10 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         setContentView(R.layout.activity_main);
 
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
-
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
         movieListAdapter = new MovieListAdapter(this, metrics);
+
         movieListRecyclerView = (RecyclerView) findViewById(R.id.rv_movie_list);
         movieListRecyclerView.setLayoutManager(layoutManager);
         movieListRecyclerView.setHasFixedSize(true);
@@ -81,16 +87,51 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
 
         movieListAdapter.setMovieInfoList(null);
 
-        switch(requestionType) {
+        switch (requestionType) {
 
             case NetworkUtils.TOP_RATED_MOVIE_URL:
-                new MovieInfoRequestingTask(movieListRecyclerView, movieListAdapter).execute(NetworkUtils.TOP_RATED_MOVIE_URL);
+                new MovieInfoRequestingTask().execute(NetworkUtils.TOP_RATED_MOVIE_URL);
                 break;
 
             case NetworkUtils.POPULAR_MOVIE_URL:
             default:
-                new MovieInfoRequestingTask(movieListRecyclerView, movieListAdapter).execute(NetworkUtils.POPULAR_MOVIE_URL);
+                new MovieInfoRequestingTask().execute(NetworkUtils.POPULAR_MOVIE_URL);
                 break;
+
+        }
+
+    }
+
+    private class MovieInfoRequestingTask extends AsyncTask<String, Void, MovieInfo[]> {
+
+        @Override
+        protected MovieInfo[] doInBackground(String... strings) {
+
+            String targetUrl = strings[0];
+            String respondedJsonString;
+            MovieInfo[] movieInfoList = null;
+
+            try {
+
+                respondedJsonString = NetworkUtils.getResponseFromHttpUrl(new URL(targetUrl));
+                movieInfoList = MovieDBJsonUtils.getMovieInfoArrayFromJsonString(respondedJsonString);
+
+            } catch (IOException | JSONException e) {
+
+                e.printStackTrace();
+
+            }
+
+            return movieInfoList;
+
+        }
+
+        @Override
+        protected void onPostExecute(MovieInfo[] movieInfoList) {
+
+            movieListAdapter.setMovieInfoList(movieInfoList);
+            movieListRecyclerView.setAdapter(movieListAdapter);
+
 
         }
 
@@ -99,11 +140,9 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
 }
 
 // polishing & bug fix
-// TODO (1) 오프라인 상태일때 처리
-// TODO (2) 그리드 레이아웃 4개씩 나오도록
-// TODO (3) 그리드 레이아웃 그림 간 마진 없애기
-// TODO (4) 위로 당기면 새로고침
-// TODO (5) 고무줄 효과 적용해서 그림이 자동으로 페이지 중앙으로 오도록
-// TODO (6) 메인 페이지 가로로 했을때 오른쪽으로 4개 나열되도록 레이아웃 수정
-// TODO (7) 영화 디테일 페이지 가로로 했을때 영화 오버뷰 오른쪽으로 빠지도록 레이아웃 수정
-// TODO (8) 리사이클러 뷰 중간에서 정렬 순서 바꿔서 로딩했을때 발생하는 런타임 에러 수정
+// TODO: 오프라인 상태일때 처리
+// TODO: 위로 당기면 새로고침
+// TODO: 고무줄 효과 적용해서 그림이 자동으로 페이지 중앙으로 오도록
+// TODO: 메인 페이지 가로로 했을때 오른쪽으로 4개 나열되도록 레이아웃 수정
+// TODO: 영화 디테일 페이지 가로로 했을때 영화 오버뷰 오른쪽으로 빠지도록 레이아웃 수정
+// TODO: 리사이클러 뷰 드래그 빠르게 했을때 버벅거림 수정
