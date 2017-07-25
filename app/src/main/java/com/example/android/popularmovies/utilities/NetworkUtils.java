f4764ad26d32706ae0463247be9d00f1da24e973
Donghoon Im <dongttang@gmail.com>
2017-07-25 11:10:28 +0900

    Optimize imports

--- app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/popularmovies/utilities/NetworkUtils.java
@@ -1,11 +1,8 @@
 package com.example.android.popularmovies.utilities;
 
-import android.net.Uri;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -42,18 +39,11 @@ public class NetworkUtils {
             }
 
         }
-        catch (Exception e) {
-
-            e.printStackTrace();
-
-        }
         finally {
 
             urlConnection.disconnect();
 
         }
-
-        return null;
 
     }
 
